6fc1c70a36a3f0716c7849d43853195d25452f72
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-05-02 12:17:22 +0530

    Constants Removed

--- frontend/src/config/axios/axios.js
+++ frontend/src/config/axios/axios.js
@@ -4,7 +4,7 @@ import { VITE_API_URL } from '../../utils/constants';
 const token = localStorage.getItem('token');
 
 const axiosInstance = axios.create({
-    baseURL: VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
         Authorization: token ? `Bearer ${token}` : null,
--- frontend/src/utils/constants.js
+++
@@ -1,1 +0,0 @@
-export const VITE_API_URL= location.hostname === "localhost" ? "http://localhost:3000" : "/dev";
